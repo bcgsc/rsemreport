97c390dd6b0ab02c688355ee535ba239299f7ac1
Zhuyi Xue (bcgsc) <alfred532008@gmail.com>
2014-08-12 11:31:24 -0700

    updated views.py and templates/rsem_report/progress_report.html for the new database structure

--- rsem_report/views.py
+++ rsem_report/views.py
@@ -1,13 +1,22 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from rsem_report.models import GSE
 
 def update_and_sort_gses(gses):
     for gse in gses:
-        gse.passed_gsms_as_list = gse.passed_gsms.split(', ')
-        gse.running_gsms_as_list = gse.running_gsms.split(', ')
-        gse.queued_gsms_as_list = gse.queued_gsms.split(', ')
-        gse.failed_gsms_as_list = gse.failed_gsms.split(', ')
+        gse.passed_gsms = gse.gsm_set.filter(status='passed')
+        gse.running_gsms = gse.gsm_set.filter(status='running')
+        gse.queued_gsms = gse.gsm_set.filter(status='queued')
+        gse.failed_gsms = gse.gsm_set.filter(status='failed')
+        gse.none_gsms = gse.gsm_set.filter(status='none')
+
+        gse.num_passed_gsms = gse.passed_gsms.count()
+        gse.num_running_gsms = gse.running_gsms.count()
+        gse.num_queued_gsms = gse.queued_gsms.count()
+        gse.num_failed_gsms = gse.failed_gsms.count()
+        gse.num_none_gsms = gse.none_gsms.count()
+
     gses = sorted(gses, key=lambda x: x.name)
     return gses
 
@@ -28,10 +37,12 @@ def not_passed_GSMs(request):
 
 def stats(request):
     gses = GSE.objects.all()
+    gses = update_and_sort_gses(gses)
     for gse in gses:
-        a, b, c, d = (gse.num_passed_gsms, gse.num_running_gsms,
-                      gse.num_queued_gsms, gse.num_failed_gsms)
-        gse.num_all_gsms = sum([a, b, c, d])
+        a, b, c, d, e = (gse.num_passed_gsms, gse.num_running_gsms,
+                         gse.num_queued_gsms, gse.num_failed_gsms,
+                         gse.num_none_gsms)
+        gse.num_all_gsms = sum([a, b, c, d, e])
         gse.passed_gsms_percentage = float(a) / gse.num_all_gsms * 100
     gses = sorted(gses, key=lambda x: (x.passed_gsms_percentage, x.name))
     return render(request, 'rsem_report/stats.html', {'gses':gses})
