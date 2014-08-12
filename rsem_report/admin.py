8e845bd20a083ce8f5cdce0a71d4f23f4e2f6db5
Zhuyi Xue (bcgsc) <alfred532008@gmail.com>
2014-08-12 12:02:42 -0700

    updated admin.py

--- rsem_report/admin.py
+++ rsem_report/admin.py
@@ -1,8 +1,31 @@
 from django.contrib import admin
-from rsem_report.models import GSE
+from rsem_report.models import GSE, Species, GSM
 
 class GSEAdmin(admin.ModelAdmin):
     list_display = ['name']
     readonly_fields = ['created', 'updated']
 
+class SpeciesAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+
+class GSMAdmin(admin.ModelAdmin):
+    def get_gse(self, obj):
+        return obj.gse.name
+    get_gse.short_description = 'GSE'
+    get_gse.admin_order_field = 'gse__name'
+
+    def get_species(self, obj):
+        return obj.species.name
+    get_species.short_description = 'Species'
+    get_species.admin_order_field = 'species__name'
+
+    list_display = ['name', 'status', 'get_gse', 'get_species']
+
+    readonly_fields = ['created', 'updated']
+
+
+
 admin.site.register(GSE, GSEAdmin)
+admin.site.register(Species, SpeciesAdmin)
+admin.site.register(GSM, GSMAdmin)
