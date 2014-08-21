154a183aafb1f9a89e591d70ea4f6ea71b60b84e
Zhuyi Xue (bcgsc) <alfred532008@gmail.com>
2014-08-21 12:49:13 -0700

    added KEY_PREFIX in CACHES of settings.py

--- rsem_report/settings.py
+++ rsem_report/settings.py
@@ -98,5 +98,7 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.memcached.PyLibMCCache',
         'LOCATION': '127.0.0.1:11211',
         'TIMEOUT': 60,
+        'KEY_PREFIX': BASE_DIR
     }
 }
+
